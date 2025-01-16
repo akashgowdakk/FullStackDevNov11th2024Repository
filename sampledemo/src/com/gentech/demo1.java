2e38c057df70583994f78f054fd6c07585748f9f
akashgowdakk <akashkk3070@gmail.com>
2025-01-16 12:43:08 +0530

    Initial Draft

---
+++ sampledemo/src/com/gentech/demo1.java
@@ -0,0 +1,14 @@
+package com.gentech;
+
+public class demo1
+{
+    public static void main (String args[])
+    {
+        int x[]={10,20,30,40,50};
+        for(int i=0;i<x.length;i++)
+        {
+            System.out.println(x[i]);
+        }
+    }
+}
+
